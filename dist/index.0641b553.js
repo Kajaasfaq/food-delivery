3b54d8b325c48aa89f9b535699406a12cdff6db1
Kajaasfaq <100942564+Kajaasfaq@users.noreply.github.com>
2023-05-15 02:36:48 +0530

    initial commit

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -35946,8 +35946,8 @@ var _reselect = require("reselect");
 // src/getDefaultMiddleware.ts
 var _reduxThunk = require("redux-thunk");
 var _reduxThunkDefault = parcelHelpers.interopDefault(_reduxThunk);
-var global = arguments[3];
 var process = require("c6d7cacc614dfba4");
+var global = arguments[3];
 var __extends = undefined && undefined.__extends || function() {
     var extendStatics = function(d, b) {
         extendStatics = Object.setPrototypeOf || ({
