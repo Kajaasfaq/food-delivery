96fd6240f612ef34dd067d50c8548d844566943a
Asfaq <100942564+Kajaasfaq@users.noreply.github.com>
2023-07-27 00:46:56 +0530

    Update contant.js

--- src/contant.js
+++ src/contant.js
@@ -735,4 +735,6 @@ export const restrautList = [
 
 export const FETCH_MENU_URL = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.517929&lng=88.38341199999999&restaurantId=`;
 
+export const FETCH_ALL_RESTAURANTS2 = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
 export const FETCH_ALL_RESTAURANTS = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&page_type=DESKTOP_WEB_LISTING";
