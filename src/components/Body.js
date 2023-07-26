04da60126f3d5e6656e3e3b479f6fc4934ef66c5
Asfaq <100942564+Kajaasfaq@users.noreply.github.com>
2023-07-26 23:21:14 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -45,11 +45,12 @@ const isOnline = useOnline()
     return (allrestaurants?.length === 0) ? (<Shimmer/>) : (
       <>
       <div className="absolute top-7 left-[580px] flex gap-5">
+      <form>
         <input  data-testid="btn-inpu" className="w-60 px-2 placeholder:text-center" type='text' placeholder='Search for Reataurant and Dish' value={searchTxt} onChange={(e) => {setSearchTxt(e.target.value)}}></input>
         <button data-testid="btn-tests" className="hover:text-slate-300 hover:underline" onClick = {() => {
          const data = filterData(searchTxt,allrestaurants);
          setFilteredRestaurants(data);
-         }}> Search</button>
+         }}> Search</button></form>
     </div>
      <div className="bg-body-colour flex flex-wrap gap-7 p-10 justify-center " data-testid="res-list">
        {filteredRestaurants.map((restaurant) => {
