3af7c5f1ea1b2c62593eb8174efdf291075b6e37
Asfaq <100942564+Kajaasfaq@users.noreply.github.com>
2023-07-26 23:51:54 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -45,9 +45,9 @@ const isOnline = useOnline()
     return (allrestaurants?.length === 0) ? (<Shimmer/>) : (
       <>
       <div className="absolute top-7 left-[580px]">
-      <Link to="/sear"><form className="flex gap-5" onClick = {(e) => {e.preventDefault()}}>
+      <form className="flex gap-5" onClick = {(e) => {e.preventDefault()}}>
         <input  data-testid="btn-inpu" className="w-60 px-2 placeholder:text-center" type='text' placeholder='Search for Reataurant and Dish' value={searchTxt} onChange={(e) => {setSearchTxt(e.target.value)}}></input>
-        <button data-testid="btn-tests" className="hover:text-slate-300 hover:underline" onClick = {() => {
+        <Link to="/sear"><button data-testid="btn-tests" className="hover:text-slate-300 hover:underline" onClick = {() => {
          const data = filterData(searchTxt,allrestaurants);
          setFilteredRestaurants(data);
          }}> Search</button></form> </Link>
